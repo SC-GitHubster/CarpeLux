0d656df2ce07a6e64ede7e55f39cf8272ad8ee9c
william ennis williamjosephclyne@outlook.com <williamjosephclyne@outlook.com>
2024-07-05 14:10:27 +0100

    added username to the users schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_04_134122) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_05_130931) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,8 +47,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_04_134122) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "bookings", "cars"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-puts test
+Car.create(make: "Toyota", model: "Corolla", year: 2021, description: "A reliable sedan.", image_url: "https://www.motortrend.com/uploads/2023/11/015-2023-Toyota-Corolla-Hybrid-in-action.jpg?w=768&width=768&q=75&format=webp", price: 20000, user_id: 1)
+Car.create(make: "Ford", model: "Mustang", year: 2022, description: "A stylish sports car.", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/1965_Shelby_Mustang_GT350_%2820984175008%29_%28cropped%29.jpg/640px-1965_Shelby_Mustang_GT350_%2820984175008%29_%28cropped%29.jpg", price: 30000, user_id: 1)
