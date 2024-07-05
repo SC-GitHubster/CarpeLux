29d1b185f383d495f3d95843db1bb8c7289ddd7e
william ennis williamjosephclyne@outlook.com <williamjosephclyne@outlook.com>
2024-07-05 13:42:59 +0100

    made changes to the applications file and added a header/css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_04_184412) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_04_134122) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,14 +27,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_04_184412) do
   end
 
   create_table "cars", force: :cascade do |t|
-    t.string "make"
     t.string "model"
+    t.string "make"
+    t.string "description"
     t.integer "year"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.string "description"
     t.decimal "price"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
