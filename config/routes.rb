2e6deb31aff94809cedc654c747d9840b9ffde18
william ennis williamjosephclyne@outlook.com <williamjosephclyne@outlook.com>
2024-07-06 14:51:35 +0100

    stuff has changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   root to: "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
-
+  
   resources :cars do
     resources :bookings, only: [:new, :create]
   end
