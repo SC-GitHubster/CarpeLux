b6422f64a6b940c09f16d3c94047299f4c575093
Sabatino Catucci <sabatinocatucci@hotmail.com>
2024-07-06 12:50:02 +0100

    Cars Index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-Car.find_or_create_by(make: "Toyota", model: "Corolla", year: 2021, description: "A reliable sedan.", image_url: "https://www.usnews.com/object/image/0000018c-5f0b-dc6c-aded-ffbf2d960000/https-cars-dms-usnews-com-static-uploads-images-auto-custom-15294-original-2024-toyota-corolla-angular-front-1.jpg?update-time=1693333947000&size=responsive640", price: 20000, user_id: 1)
+User.destroy_all
 
-Car.find_or_create_by(make: "Lexus", model: "ES", year: 2022, description: "A midsize luxury sedan with a smooth ride.", image_url: "https://mag.lexus.co.uk/wp-content/uploads/sites/3/2021/03/Lexus-ES-300-fourth-05-696x416-1.jpg", price: 43000, user_id: 3)
+5.times do |i|
+  User.create!(username: "user_#{i}", email: "user_#{i}@email.com", password: "user_#{i}")
+end
+
+Car.create!(make: "Toyota", model: "Corolla", year: 2021, description: "A reliable sedan.", image_url: "https://www.usnews.com/object/image/0000018c-5f0b-dc6c-aded-ffbf2d960000/https-cars-dms-usnews-com-static-uploads-images-auto-custom-15294-original-2024-toyota-corolla-angular-front-1.jpg?update-time=1693333947000&size=responsive640", user: User.all.sample)
+
+Car.create!(make: "Lexus", model: "ES", year: 2022, description: "A midsize luxury sedan with a smooth ride.", image_url: "https://mag.lexus.co.uk/wp-content/uploads/sites/3/2021/03/Lexus-ES-300-fourth-05-696x416-1.jpg", user: User.all.sample)
